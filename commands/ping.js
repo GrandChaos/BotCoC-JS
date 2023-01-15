31e7ed2cc2663db058e4a6e9b824782096f2c545
GrandChaos <v.grand.chaos.v@gmail.com>
2023-01-15 19:04:24 +0300

    Проверка внешних интеграций при /ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,7 @@
 module.exports = async (bot, clash, message, args, argsF) => {
   console.log(`API Latency is ${Math.round(bot.ws.ping)}ms\n${Date()}\n`);
   message.reply(`I'm here!\n\Latency is ${Date.now() - message.createdTimestamp}ms.\n\API Latency is ${Math.round(bot.ws.ping)}ms`)
+  require('../update/checkApi')(bot, clash);
 };
 
 module.exports.names = ["ping"]
